c1111346ae67bbf4e1fc9a29f49369e4dfaedb66
quanyuan <quanyuan1895@gmail.com>
2025-11-04 10:33:59 +0800

    sea_wea_dashboard

---
+++ _next/static/W32Ox2r4HV54ljR0EycJ_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/620-f2620aa621bfdf5e.js","static/chunks/pages/index-4c8e93d57d2cd4f5.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
